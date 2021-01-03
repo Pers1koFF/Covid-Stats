52e5249aaadf3b05f1d9a45778b38e3b0ec2b087
Pers1koFF <66518951+Pers1koFF@users.noreply.github.com>
2021-01-03 21:55:28 +1000

    Add files via upload

---
+++ Covid_stats/JsonResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid_stats
+{
+    public class JsonResponse
+    {
+        public int cases { get; set; }
+        public int recovered { get; set; }
+        public int deaths { get; set; }
+    }
+}
---
+++ Covid_stats/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Windows;
+
+namespace Covid_stats
+{
+    public partial class MainWindow : Window{
+        public MainWindow(){InitializeComponent();label_cases.Opacity = 0;label_deaths.Opacity = 0;label_recovered.Opacity = 0;}
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (textbox.Text is null || textbox.Text == "" || textbox.Text == " ")
+                MessageBox.Show("Вы не указали страну!", "Тупой шоли", MessageBoxButton.OK, MessageBoxImage.Error);
+            else{label_cases.Opacity = 0;label_deaths.Opacity = 0;label_recovered.Opacity = 0;
+                HttpClient client = new HttpClient();
+                string json = "";
+                try
+                {
+                    json = await client.GetStringAsync($"https://corona.lmao.ninja/v2/countries/{textbox.Text}");
+                }catch(System.Net.Http.HttpRequestException ex) { MessageBox.Show("Неверная страна.", "Error");
+                    return;
+                }
+                JsonResponse convertedJsonData = JsonConvert.DeserializeObject<JsonResponse>(json);cases.Content = convertedJsonData.cases + " чел";recovered.Content = convertedJsonData.recovered + " чел";ded_insaid.Content = convertedJsonData.deaths + " чел";
+
+                if ((bool)check_afterUse.IsChecked)textbox.Text = "";
+                label_cases.Opacity = 100;label_deaths.Opacity = 100;label_recovered.Opacity = 100;}}}}
